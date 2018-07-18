e8d2ebb61050ac27b279340c955df643481f9c1f
Kenneth Korcal <kkorcal@pandora.com>
2018-07-17 20:55:50 -0700

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,20 @@ let list2 = [2, 1, 0, 3, 0, 2, 1, 3, 2, 3];
 let list3 = [2, 1, 0, 3, 0];
 const list = list2;
 const MAX_Z = 30;
-const MIN_DIST = 0.25
-const MAX_DIST = 0.80;
+const MAX_DIST = 0.25;
 const INTERVAL = 0.05;
+// let initialRender = true;
+
+const hoverEffectClasses = {
+  noEffect: 'art__child',
+  initialFade: 'art__child art__child--initial-fade-in',
+  hoverOut: 'art__child art__child--hover-out'
+};
+
 const initialState = {
   list,
   isHovered: false,
+  prevHoverIdx: null,
   hoverIdx: null,
   listPositions: list.map((item, idx) => {
     return {
@@ -33,10 +41,14 @@ class App extends Component {
 
   handleMouseEnter = hoverIdx => {
     // TODO: displacement happens when flex basis width is less than the source width
-    const art = this.container.current.querySelector('.art:nth-child(2)');
-    const artWidth = art.getBoundingClientRect().width;
+    const listSize = this.state.list.length;
+    const container = this.container.current;
+    const containerWidth = container.getBoundingClientRect().width;
+    const artChild = container.querySelector('.art__child');
+    const artWidth = artChild.getBoundingClientRect().width;
+    const totalArtWidth = artWidth * listSize;
 
-    let listPositions = list.map((item, idx) => {
+    let listPositions = this.state.list.map((item, idx) => {
       if (idx === hoverIdx) {
         return {
           zIndex: MAX_Z,
@@ -47,8 +59,10 @@ class App extends Component {
       const diff = Math.abs(idx - hoverIdx);
       let dist = 0;
 
-      if (idx !== 0 && idx !== list.length - 1) {
-        dist = artWidth * (MIN_DIST + ((diff - 1) * INTERVAL));
+      if (idx !== 0 && idx !== this.state.list.length - 1) {
+        if (totalArtWidth > containerWidth + artWidth) {
+          dist = artWidth * MAX_DIST;
+        }
 
         if (idx < hoverIdx) dist = -1 * dist;
       }
@@ -59,26 +73,32 @@ class App extends Component {
       };
     });
 
-    this.setState({ isHovered: true, hoverIdx, listPositions });
-  };
-
-  handleMouseLeave = () => {
     const list = [...this.state.list];
-    this.setState(Object.assign({}, initialState, { list }));
+    this.setState(Object.assign({}, initialState, { isHovered: true, list, hoverIdx, listPositions }));
   };
 
-  // TODO
+  handleMouseLeave = hoverIdx => {
+    const list = [...this.state.list];
+    const prevHoverIdx = hoverIdx;
+    this.setState(Object.assign({}, initialState, { list, prevHoverIdx }));
+  };
+
   addPuppy = () => {
-    const list = [0, ...this.state.list];
+    // TODO: add variables
+    let idx = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+    const list = [idx, ...this.state.list];
     this.setState({ list });
   };
 
-  // TODO
-  removePuppy = () => {};
+  removePuppy = () => {
+    const list = [...this.state.list];
+    list.pop();
+    this.setState({ list });
+  };
 
   render() {
     return (
-      <div className="App">
+      <div className="App initial-fade-in">
         <div className="buttons">
           <button onClick={this.addPuppy}>ADD PUPPY</button>
           <button onClick={this.removePuppy}>REMOVE PUPPY</button>
@@ -86,16 +106,28 @@ class App extends Component {
         <div className="RecentlyPlayed">
           <div className="container" ref={this.container}>
             {this.state.list.map((dog, idx) => {
-              const styles = this.state.listPositions[idx];
+              const artChildstyles = this.state.listPositions[idx];
+              const artStyles = this.state.isHovered ? {overflow: 'visible'} : {overflow: 'hidden'};
+              let hoverEffectClass = hoverEffectClasses.noEffect;
+
+              if (idx === this.state.prevHoverIdx) {
+                hoverEffectClass = hoverEffectClasses.hoverOut;
+              }
+
+              // if (initialRender) {
+              //   hoverEffectClass = hoverEffectClasses.initialFade;
+              //   initialRender = false;
+              // }
 
               return (
                 <div
                   key={idx}
                   className="art"
                   onMouseEnter={() => this.handleMouseEnter(idx)}
-                  onMouseLeave={this.handleMouseLeave}
+                  onMouseLeave={() => this.handleMouseLeave(idx)}
+                  style={artStyles}
                 >
-                  <div className="art__child" style={styles}>
+                  <div className={hoverEffectClass} style={artChildstyles}>
                     <img src={dogs[dog]} alt="" />
                   </div>
                 </div>
